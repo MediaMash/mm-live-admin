23ebc8d09bd5f9e2596d27bd3726bb8e4f96181d
Greg Lind <greglind@Gregs-Air.home>
2022-03-25 15:47:40 -0700

    remove webcam

--- video/stream_views.py
+++ video/stream_views.py
@@ -175,34 +175,6 @@ class LiveStreamDelete(DeleteView):
 
     form_class = LiveStreamForm
 
-# LIVESTREAM WEBCAM
-import cv2
-import numpy as np
-class VideoCamera(object):
-    def __init__(self):
-        self.cap = cv2.VideoCapture(0)
-    def __del__(self):
-        self.cap.release()
-    def get_frame(self):
-        ret, frame = self.cap.read()
-        frame_flip = cv2.flip(frame, 1)
-        ret, frame = cv2.imencode('.jpg', frame_flip)
-        return frame.tobytes()
-
-from django.http.response import StreamingHttpResponse
-from streamApp.camera import VideoCamera
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-def video_stream(request):
-    return StreamingHttpResponse(gen(VideoCamera()),
-                    content_type='multipart/x-mixed-replace; boundary=frame')
-
-
-
 
 def logout_view(request):
     """
